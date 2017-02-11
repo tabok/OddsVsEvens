00cad437ba6b445e022e0858a3f7453a5a746700
pietrooha02 <pietrooha02@gmail.com>
2017-02-11 23:55:03 +0100

    Added new function and small code changes

--- OddsVsEven.php
+++ OddsVsEven.php
@@ -1,8 +1,9 @@
 <?php
+
 /**
-** Procedural function which count number of zeros and number of ones.
-** Zeros represent evens soldiers and ones represent odds soldiers.
-**/
+*  Procedural function which count number of zeros and number of ones.
+*  Zeros represent evens soldiers and ones represent odds soldiers.
+*/
 function battle($battle_field_configuration)
 {
 	$number_of_all_positive_zeros = 0;
@@ -10,76 +11,94 @@ function battle($battle_field_configuration)
 	$number_of_all_positive_ones = 0;
 	$number_of_all_negative_ones = 0;
 	$battle_field_conf_tab_size = count($battle_field_configuration);
-
-	for($i = 0; $i < $battle_field_conf_tab_size; $i++)
+	
+	if(check_input($battle_field_configuration))
 	{
-		// evens soldiers
-		if($battle_field_configuration[$i] % 2 == 0)
-		{
-			if($battle_field_configuration[$i] >= 0)
+		for($i = 0; $i < $battle_field_conf_tab_size; $i++)
+		{	
+			// evens soldiers
+			if($battle_field_configuration[$i] % 2 == 0)
 			{
-				$binary_representation = decbin($battle_field_configuration[$i]);
-				// number of positive zeros
-				$number_of_positive_zeros = substr_count($binary_representation, '0');
-				$number_of_all_positive_zeros = $number_of_all_positive_zeros + $number_of_positive_zeros;
+				if($battle_field_configuration[$i] >= 0)
+				{
+					$binary_representation = decbin($battle_field_configuration[$i]);
+					// number of positive zeros
+					$number_of_positive_zeros = substr_count($binary_representation, '0');
+					$number_of_all_positive_zeros = $number_of_all_positive_zeros + $number_of_positive_zeros;
+				}
+				else
+				{
+					$abs_number = abs($battle_field_configuration[$i]);
+					$binary_representation = decbin($abs_number);
+					// number of negative zeros
+					$number_of_negative_zeros = substr_count($binary_representation, '0');
+					$number_of_all_negative_zeros = $number_of_all_negative_zeros + $number_of_negative_zeros;
+				}
 			}
+			// odds soldiers
 			else
 			{
-				$abs_number = abs($battle_field_configuration[$i]);
-				$binary_representation = decbin($abs_number);
-				// number of negative zeros
-				$number_of_negative_zeros = substr_count($binary_representation, '0');
-				$number_of_all_negative_zeros = $number_of_all_negative_zeros + $number_of_negative_zeros;
+				if($battle_field_configuration[$i] >= 0)
+				{
+					$binary_representation = decbin($battle_field_configuration[$i]);
+					// number of positive ones
+					$number_of_positive_ones = substr_count($binary_representation, '1');
+					$number_of_all_positive_ones = $number_of_all_positive_ones + $number_of_positive_ones;
+				}
+				else
+				{
+					$abs_number = abs($battle_field_configuration[$i]);
+					$binary_representation = decbin($abs_number);
+					// number of negative ones
+					$number_of_negative_ones = substr_count($binary_representation, '1');
+					$number_of_all_negative_ones = $number_of_all_negative_ones + $number_of_negative_ones;
+				}
 			}
 		}
-		// odds soldiers
-		else
-		{
-			if($battle_field_configuration[$i] >= 0)
-			{
-				$binary_representation = decbin($battle_field_configuration[$i]);
-				// number of positive ones
-				$number_of_positive_ones = substr_count($binary_representation, '1');
-				$number_of_all_positive_ones = $number_of_all_positive_ones + $number_of_positive_ones;
-			}
-			else
-			{
-				$abs_number = abs($battle_field_configuration[$i]);
-				$binary_representation = decbin($abs_number);
-				// number of negative ones
-				$number_of_negative_ones = substr_count($binary_representation, '1');
-				$number_of_all_negative_ones = $number_of_all_negative_ones + $number_of_negative_ones;
-			}
-		}
+		$evens_result = $number_of_all_positive_zeros - $number_of_all_negative_zeros; 
+		$odds_result = $number_of_all_positive_ones - $number_of_all_negative_ones;
+		battle_result($evens_result, $odds_result);
 	}
-	battle_result($number_of_all_positive_zeros, $number_of_all_negative_zeros, $number_of_all_positive_ones, $number_of_all_negative_ones);
+	else
+	{
+		echo 'incorrect input<br>';
+	}
 }
 
 /**
-** Procedural function which print battle result.
-**/
-function battle_result($number_of_all_positive_zeros, $number_of_all_negative_zeros, $number_of_all_positive_ones, $number_of_all_negative_ones)
+*  Procedural function which check input array.
+*/
+function check_input($battle_field_configuration)
 {
-	$number_of_all_zeros = $number_of_all_positive_zeros - $number_of_all_negative_zeros;
-	$number_of_all_ones = $number_of_all_positive_ones - $number_of_all_negative_ones;
-
-	if($number_of_all_ones > $number_of_all_zeros)
+	foreach ($battle_field_configuration as $one_number)
 	{
-		echo
-			'odds win' . ' ' . $number_of_all_positive_ones . ' - ' . $number_of_all_negative_ones .
-			' VS ' . $number_of_all_positive_zeros. ' - ' . $number_of_all_negative_zeros . '<br>';
+		if (!is_int($one_number))
+		{
+        		return false;
+    		}
 	}
-	else if($number_of_all_ones < $number_of_all_zeros)
+	return true;
+}
+
+/**
+*  Procedural function which print battle result.
+*/
+function battle_result($evens_result, $odds_result)
+{
+	if($odds_result > $evens_result)
 	{
 		echo
-			'evens win' . ' ' . $number_of_all_positive_ones . ' - ' . $number_of_all_negative_ones .
-			' VS ' . $number_of_all_positive_zeros. ' - ' . $number_of_all_negative_zeros . '<br>';
+			'odds win' . ' ' . $odds_result . ' vs ' . $evens_result . '<br>';
+	}
+	else if($odds_result < $evens_result)
+	{
+		echo
+			'evens win' . ' ' . $odds_result . ' vs ' . $evens_result . '<br>';
 	}
 	else
 	{
 		echo
-			'tie' . ' ' . $number_of_all_positive_ones . ' - ' . $number_of_all_negative_ones .
-			' VS ' . $number_of_all_positive_zeros. ' - ' . $number_of_all_negative_zeros . '<br>';
+			'tie' . ' ' . $odds_result . ' vs ' . $evens_result . '<br>';
 	}
 }
 
@@ -90,5 +109,9 @@ battle([21, -3, 20]);
 battle([7, -3, -14, 6]);
 battle([23, -3, 32, -24]);
 battle([128, -128, 31, -31]);
+battle([]);
+battle([10, 23, 22, '1b', '0b', 3]);
+battle([10, 23 , '1f', 22, '1b', '0b', 3]);
+battle([12, 21, -2, 33, 14]);
 
 ?>
